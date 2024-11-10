89338d1d84da432ba35c33216c8b5771dbcf10b2
GBiondo1310 <giacomo.biondelli@gmail.com>
2024-11-10 21:41:47 +0100

    Initial commit - dom 10 nov 2024, 21:41:47, CET

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+from test_suite import Suite
+
+# ===== EXAMPLE ===== #
+
+# try:
+#     from tests import test_example1, test_example2
+# except Exception:
+#     import test_example1
+#     import test_example2
+
+# suite = Suite(test_example1, test_example2)
+
+# ===== EXAMPLE ===== #
+
+suite = Suite()
